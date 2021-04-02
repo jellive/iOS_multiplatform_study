05438c1ad85d8a4bf0aac4ba3d58b77dd23cf54b
PD Jell <mystral7@naver.com>
2021-04-02 11:25:59 +0900

    init commit.

---
+++ Shared/test_multiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_multiplatformApp.swift
+//  Shared
+//
+//  Created by YooHG on 2021/04/02.
+//
+
+import SwiftUI
+
+@main
+struct test_multiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
